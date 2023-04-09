4312fcd46da04e5bf59a30cb30ea65c9bdbec254
Garry <garryisrael02@gmail.com>
2023-04-09 06:54:46 -0400

    Explores First Class Function

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+function area(num){
+  return num*num;
+}
+
 function receivesAFunction(area){
   const side =3;
   area(side);
+  return "Mission accomplished";
 }
 
 function returnsANamedFunction(){
-  addNumber = (num) => num +3;
+  addNumber = num => num +3;
   return addNumber;
 }
 
 function returnsAnAnonymousFunction(){
-  return (function (){console("I am happy")});
+  return function (){
+    console.log("I\'m happy");
+  };
 }
